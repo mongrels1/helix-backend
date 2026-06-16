b58b94b2104f0c346385666b315f5bb3fb06fec8
John Edwards <support@helixlms.com>
2026-06-16 15:02:44 -0400

    Set CORS for Hostinger frontend

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from '@common/filters/http-exception.filter';
-
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: [
+      'https://helixlms.com',
+      'https://96e5c841-3f5e-4845-b48b-58d2426dac3e.app-preview.com',
+    ],
+    credentials: true,
+  });
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,9 +19,7 @@ async function bootstrap(): Promise<void> {
       transform: true,
     }),
   );
-
   const port = parseInt(process.env.PORT ?? '3000', 10);
   await app.listen(port);
 }
-
 void bootstrap();
